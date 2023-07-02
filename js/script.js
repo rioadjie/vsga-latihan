e2067964c5ebb7366125d81e677ef76382f55a39
Rio Adjie Wiguna <rioadjie100301@gmail.com>
2023-07-03 04:01:58 +0700

    Login Page

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const pwShowHide = document.querySelectorAll(".pw_hide");
+
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("fa-eye-slash", "fa-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("fa-eye", "fa-eye-slash");
+    }
+  });
+});
+
